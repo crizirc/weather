a58043a4fb2574b0f6fabb4c9fc328dd8dc9fd56
Camilo <Camilo.ReinaIzykowski@spreadgroup.com>
2025-04-02 14:07:32 +0200

    Zwischenspeicher nur für 10 minuten

--- src/main/kotlin/net/sprd/internship/weather/configuration/CachingConfig.kt
+++ src/main/kotlin/net/sprd/internship/weather/configuration/CachingConfig.kt
@@ -1,14 +1,26 @@
 package net.sprd.internship.weather.configuration
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
 
 @Configuration
 @EnableCaching
 class CachingConfig {
-    fun cacheManager(): CacheManager {
-        return ConcurrentMapCacheManager("getCurrentWeather")
+    @Bean
+    fun caffeineConfig(): Caffeine<Any,Any> {
+        return Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.MINUTES)
+    }
+
+    @Bean
+    fun cacheManager(caffeine: Caffeine<Any, Any>): CacheManager {
+        val caffeineCacheManager = CaffeineCacheManager()
+        caffeineCacheManager.setCaffeine(caffeine)
+        return caffeineCacheManager
     }
 }
