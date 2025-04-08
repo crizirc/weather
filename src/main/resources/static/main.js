f92aaf7c716d65c9672dbc68914952ec7b8ecfeb
Camilo <Camilo.ReinaIzykowski@spreadgroup.com>
2025-04-08 15:14:24 +0200

    zeigt wetter nach browserstandort an

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -8,18 +8,22 @@ function success(pos) {
     const crd = pos.coords;
 
     console.log("Your current position is:");
-    console.log(`Latitude : \$\{crd.latitude\}`);
-    console.log(`Longitude: \$\{crd.longitude\}`);
-    console.log(`More or less \$\{crd.accuracy\} meters.`);
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    let href = window.location.href;
+    if(!href.includes("lat") || !href.includes("long")) {
+        href += `?lat=${crd.latitude}&long=${crd.longitude}`;
+        window.location.replace(href);
+    }
 }
 
 function error(err) {
-    console.warn(`ERROR(\$\{err.code\}): \$\{err.message\}`);
+    console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
 navigator.geolocation.getCurrentPosition(success, error, options);
-
-
+//------------------
 console.log("start register service worker");
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
